b554f01652719c4c6bb0fb8ab3b05dc1a852dea6
Imran Ali <ranaimranali2210@gmail.com>
2026-03-16 23:33:05 -0700

    authentication

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,13 @@ use App\Http\Controllers\Admin\AdminController;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('login', [AdminController::class, 'create'])->name('admin.login');   
+// Route::get('login', [AdminController::class, 'create'])->name('admin.login');   
 
-// Route::resource('admin/dashboard', AdminController::class);->only(['index']);
-Route::get('admin/dashboard', [AdminController::class, 'index'])->name('admin.dashboard.index');
+// // Route::resource('admin/dashboard', AdminController::class);->only(['index']);
+// Route::get('admin/dashboard', [AdminController::class, 'index'])->name('admin.dashboard.index');
+Route::prefix('admin') ->group(function () {
+    Route::get('login', [AdminController::class, 'create'])->name('admin.login');   
+    Route::group(['middleware' => 'admin'], function () {
+        Route::resource('dashboard', AdminController::class)->only(['index']);
+    });
+});
